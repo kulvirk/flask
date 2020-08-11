22540504cfe02456c2356a8f5e27d26213fa8a20
Kulvir Kaur <kulvirk@users.noreply.github.com>
2020-08-11 16:09:20 +1000

    Delete app3.py

--- app3.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, url_for, request, render_template
-
-app=Flask(__name__)
-@app.route('/<V1>')
-def hello(V1):
-	return render_template('Homepage.html', name=V1)
-if __name__=='__main__':
-	app.run()
